785af14a87770fee7c210d3098154c4d8727e8d3
Raymond Oung <r.oung@hapi-robo.com>
2021-07-14 15:53:15 +0900

    Update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for Localization -------------------------------------------------
+gettext_uuid = True # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-gettext_uuid
+gettext_compact = False # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-gettext_compact
